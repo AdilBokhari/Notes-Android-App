3da865c75b8aac289dec2429cabace7dbe43bcd8
Adil <bokhariadil@gmail.com>
2023-06-22 16:21:55 +0500

    Today is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:codecampapp/views/login_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +26,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: const Text("Home Page"),
         backgroundColor: Colors.blue[50],
         foregroundColor: Colors.black,
       ),
@@ -36,13 +37,44 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              print(FirebaseAuth.instance.currentUser);
-              return const Text("Done");
+              /*final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print("User Email is verifed");
+              } else {
+                return const VerifyEmail();
+              }
+              return const Text("Done");*/
+              return const LoginView();
             default:
               return const Text("Loading...");
           }
         },
       ),
+    );
+  }
+}
+
+class VerifyEmail extends StatefulWidget {
+  const VerifyEmail({super.key});
+
+  @override
+  State<VerifyEmail> createState() => _VerifyEmailState();
+}
+
+class _VerifyEmailState extends State<VerifyEmail> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("Please verify email address"),
+        TextButton(
+          onPressed: () async {
+            final user = FirebaseAuth.instance.currentUser;
+            await user?.sendEmailVerification();
+          },
+          child: const Text("Send Email Verification"),
+        )
+      ],
     );
   }
 }
